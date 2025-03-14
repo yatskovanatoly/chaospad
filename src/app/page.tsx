656415330fb7c735fe7b352d932341920d7b6907
Anatoly <yatskovanatoly@gmail.com>
2025-03-15 01:28:26 +0300

    Minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import ChaosPad from '@/components/ChaosPad'
-import GlowEffect from '@/components/GlowFx'
+'use client'
 
-const Page = () => {
-	return (
-		<>
-			<GlowEffect />
-			<ChaosPad />
-		</>
-	)
-}
+import ChaosPad from '@/components/ChaosPad'
+import GlowFx from '@/components/GlowFx'
+
+const Page = () => (
+	<div>
+		<ChaosPad />
+		<GlowFx />
+	</div>
+)
 
 export default Page
