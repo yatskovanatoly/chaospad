ea261ab4cfb37e9a0a989d3c5782c1affcb69f1c
Anatoly <yatskovanatoly@gmail.com>
2025-03-18 07:22:12 +0300

    Minor minMax freq

--- src/helpers/getSoundParams.ts
+++ src/helpers/getSoundParams.ts
@@ -1,11 +1,19 @@
 export const getSoundParamsFromXY = (clientX: number, clientY: number) => {
-	const minFreq = 100
-	const maxFreq = 1000
+	const minFreq = 256
+	const maxFreq = 512
 
-	const x = clientX / window.innerWidth
-	const freq = minFreq * Math.pow(maxFreq / minFreq, x) // Logarithmic scale
+	// Get the bounds of the element
+	const { left, top, width, height } =
+		window.document.body.getBoundingClientRect()
 
-	const y = clientY / window.innerHeight
+	// Normalize the clientX and clientY values based on the element's position and size
+	const x = (clientX - left) / width // Normalize x relative to the element's width
+	const y = (clientY - top) / height // Normalize y relative to the element's height
+
+	// Apply logarithmic scaling for frequency based on normalized x value
+	const freq = minFreq * Math.pow(maxFreq / minFreq, x)
+
+	// Amplitude decreases as y increases, within the range [0, 1]
 	const amp = 1 - y
 
 	return { freq, amp, x, y }
