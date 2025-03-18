50a5a38e25b6638217a2e8e0b11548eab91467d9
Anatoly <yatskovanatoly@gmail.com>
2025-03-18 05:46:01 +0300

    Handle remote helper

---
+++ src/helpers/handleRemoteAudio.ts
@@ -0,0 +1,53 @@
+import { RemoteUserType } from '@/components/hooks/useChaosWs'
+import { createSoundChain } from './createSoundChain'
+import { getSoundParamsFromXY } from './getSoundParams'
+
+const handleRemoteEvent = ({
+	userId,
+	type,
+	x,
+	y,
+	ctx,
+	remoteUsersRef,
+}: RemoteProps) => {
+	if (!ctx) {
+		ctx = new AudioContext()
+	}
+
+	const { freq, amp, x: clientX, y: clientY } = getSoundParamsFromXY(x, y)
+
+	if (type === 'start') {
+		const { osc, gain } = createSoundChain(ctx, clientX, clientY)
+		osc.start()
+		remoteUsersRef[userId] = { osc, gain }
+	}
+
+	if (type === 'move') {
+		const user = remoteUsersRef[userId]
+		if (user) {
+			user.osc.frequency.setValueAtTime(freq, ctx.currentTime)
+			user.gain.gain.cancelScheduledValues(ctx.currentTime)
+			user.gain.gain.setValueAtTime(amp * 0.5, ctx.currentTime)
+		}
+	}
+
+	if (type === 'stop') {
+		const user = remoteUsersRef[userId]
+		if (user) {
+			user.gain.gain.linearRampToValueAtTime(0, ctx.currentTime + 0.5)
+			user.osc.stop(ctx.currentTime + 0.6)
+			delete remoteUsersRef[userId]
+		}
+	}
+}
+
+type RemoteProps = {
+	userId: string
+	type: string
+	x: number
+	y: number
+	ctx: AudioContext | null
+	remoteUsersRef: RemoteUserType
+}
+
+export default handleRemoteEvent
