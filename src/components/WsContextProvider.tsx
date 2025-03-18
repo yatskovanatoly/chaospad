14ddd30328f611debda4cf7268b45b5ada7f4e51
Anatoly <yatskovanatoly@gmail.com>
2025-03-18 18:55:10 +0300

    Cleaner wsContext

--- src/components/WsContextProvider.tsx
+++ src/components/WsContextProvider.tsx
@@ -3,6 +3,7 @@
 import { WS_URL } from '@/config'
 import { useEffect, useMemo, useRef, useState } from 'react'
 import { WebSocketContext } from './WsContext'
+import { colors, getColorForUser, getUserId } from '@/helpers/getUserParams'
 
 const WebSocketProvider = ({ children }: { children: React.ReactNode }) => {
 	const [pos, setPos] = useState({ x: 0, y: 0 })
@@ -45,35 +46,6 @@ const WebSocketProvider = ({ children }: { children: React.ReactNode }) => {
 	)
 }
 
-const getUserId = () => {
-	try {
-		return crypto.randomUUID()
-	} catch (e) {
-		console.log(e)
-		return Math.random().toFixed()
-	}
-}
-
-const colors = [
-	'border-blue-500',
-	'border-red-500',
-	'border-green-500',
-	'border-yellow-500',
-	'border-purple-500',
-]
-
 export type MotionType = 'start' | 'stop' | 'move'
-
-// Hash function to pick a consistent color based on user ID
-const getColorForUser = (id: string | undefined) => {
-	if (!id) return undefined
-
-	let hash = 0
-	for (let i = 0; i < id.length; i++) {
-		hash = id.charCodeAt(i) + ((hash << 5) - hash)
-	}
-	const index = Math.abs(hash) % colors.length
-	return colors[index]
-}
 
 export default WebSocketProvider
---
+++ src/helpers/getUserParams.ts
@@ -0,0 +1,27 @@
+export const colors = [
+	'border-blue-500',
+	'border-red-500',
+	'border-green-500',
+	'border-yellow-500',
+	'border-purple-500',
+]
+
+export const getColorForUser = (id: string | undefined) => {
+	if (!id) return undefined
+
+	let hash = 0
+	for (let i = 0; i < id.length; i++) {
+		hash = id.charCodeAt(i) + ((hash << 5) - hash)
+	}
+	const index = Math.abs(hash) % colors.length
+	return colors[index]
+}
+
+export const getUserId = () => {
+	try {
+		return crypto.randomUUID()
+	} catch (e) {
+		console.log(e)
+		return Math.random().toFixed()
+	}
+}
