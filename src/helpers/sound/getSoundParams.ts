96a0228ccbde142431ca44f4a237ba57a7d0ddca
Anatoly <yatskovanatoly@gmail.com>
2025-10-17 15:09:37 +0300

    fix: getSoundsParams

--- src/helpers/sound/getSoundParams.ts
+++ src/helpers/sound/getSoundParams.ts
@@ -2,8 +2,8 @@ export const getSoundParamsFromXY = (clientX: number, clientY: number) => {
 	const minFreq = 256
 	const maxFreq = 512
 
-	const x = (clientX / window.innerWidth) * 1.5 // Normalize x relative to the element's width
-	const y = (clientY / window.innerHeight) * 1.5 // Normalize y relative to the element's height
+	const x = clientX / window.innerWidth // Normalize x relative to the element's width
+	const y = clientY / window.innerHeight // Normalize y relative to the element's height
 
 	// Apply logarithmic scaling for frequency based on normalized x value
 	const freq = minFreq * Math.pow(maxFreq / minFreq, x)
